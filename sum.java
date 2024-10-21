b075d62467a315aaa1f46418c2b5d504694f76fd
Pratik45ro <pratik.17252@sakec.ac.in>
2024-10-21 18:18:02 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,15 +1,19 @@
-public class sum{
-    public static void main(String[] args){
-        if (args.length != 2){
-            System.out.println("Please enter two integer arguments");
+public class sum {
+    public static void main(String[] args) {
+        // Check if two arguments are passed
+        if (args.length != 2) {
+            System.out.println("Please provide exactly two numbers as arguments.");
             return;
         }
 
+        // Parse the command-line arguments to integers
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
 
+        // Calculate the sum
         int sum = a + b;
 
-        System.out.println(a + " + " + b + " = " + sum);
-    }
+        // Display the result
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
 }
