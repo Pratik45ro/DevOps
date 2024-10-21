7f91beb95d604d92fb0456c3df08e95c62560934
Pratik45ro <pratik.17252@sakec.ac.in>
2024-10-21 20:02:27 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+    public static void main(String[] args) {
+        // Hardcoded values for the numbers
+        int num1 = 10;  // Example value for the first number
+        int num2 = 20;  // Example value for the second number
+
+        // Adding the numbers
+        int sum = num1 + num2;
+
+        // Output the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
